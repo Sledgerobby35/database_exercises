08ffeb968cb714883264fea4f17ecd5c864faace
Sledgerobby35 <sledge.robby35@gmail.com>
2021-07-26 15:04:14 -0500

    add tables exercises on mySQL

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100),
+    name VARCHAR(200),
+    release_date DATE,
+    sales float,
+    genre varchar(50),
+    PRIMARY KEY (id)
+);
